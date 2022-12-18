2e71ce4f1be9aa0227b6d05e3dad054b2385036a
ArgunX <49057613+ArgunX@users.noreply.github.com>
2022-12-18 14:50:08 +0300

    Update event.go

--- event.go
+++ event.go
@@ -1,16 +1,22 @@
 package calendar
 
+import (
+	"errors"
+	"unicode/utf8"
+)
+
 type Event struct {
 	title string
 	Date
 }
+
 func (e *Event) Title() string {
-return e.title
+	return e.title
 }
 func (e *Event) SetTitle(title string) error {
-if utf8.RuneCountInString(title) > 30 {
- return errors.New("invalid title")
-}
-e.title = title
-return nil
+	if utf8.RuneCountInString(title) > 30 {
+		return errors.New("invalid title")
+	}
+	e.title = title
+	return nil
 }
